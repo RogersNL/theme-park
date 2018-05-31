a8f13c09b20ffcfdfa00bd01381dd987da327ff4
Alex Bunnell and Nick Rogers <alex@>
2018-05-31 14:46:49 -0700

    Change functionality to all rides being displayed from the start, however the inputted age now highlights rides that the user is eligable.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
 
   var height = parseInt(prompt("How tall are you in Inches?"));
   if (height >= 60) {
-    $('#normal-ride').show();
-    $('#tall-ride').show();
-    $('#extra-tall').show();
+    $('#normal-ride').addClass("highlighted");
+    $('#tall-ride').addClass("highlighted");
+    $('#extra-tall').addClass("highlighted");
   } else if (height >= 56) {
-    $('#normal-ride').show();
-    $('#tall-ride').show();
+    $('#normal-ride').addClass("highlighted");
+    $('#tall-ride').addClass("highlighted");
   } else if(height > 48) {
-    $('#normal-ride').show();
+    $('#normal-ride').addClass("highlighted");
   } else
-    $('#kids-ride').show();
-    $('#normal-ride').show();
+    $('#kids-ride').addClass("highlighted");
+    $('#normal-ride').addClass("highlighted");
 });
